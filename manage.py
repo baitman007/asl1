d6628b1584a485d9274d76dff505007be7744346
Bhararth Rajiv <48879929+baitman007@users.noreply.github.com>
2023-03-17 21:22:38 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management.commands.runserver import Command as runserver
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A2SL.settings')
@@ -14,6 +14,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    runserver.default_port = os.environ.get('PORT', 8000)
+    runserver.default_addr = '0.0.0.0'
     execute_from_command_line(sys.argv)
 
 
